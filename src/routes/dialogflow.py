5b213f2e4aa50a8723de2d0fb5f9883c0e8596a8
imrooteodoro <adelsonteodoro67@gmail.com>
2024-07-04 14:36:35 -0300

    feat: cria  expressão regular para o bot exibir em urls em formatos de url e importa funcao para criar session_id

--- src/routes/dialogflow.py
+++ src/routes/dialogflow.py
@@ -3,9 +3,12 @@ from flask import Flask, request, jsonify, app, render_template
 import subprocess
 from auth.auth import get_access_token
 from services.payload import send_message_to_dialogflow
+from services.payload import user_id
+from utils.generate_id import session_id
+import re
+
 
 historico_de_mensagens = []
-
 def botpost(app):
     @app.route('/dialogflow', methods=['POST'])
     def enviar_mensagem_para_dialogflow():
@@ -18,7 +21,14 @@ def botpost(app):
             fulfillment_message = resultado["queryResult"]["fulfillmentMessages"][0]
             if "text" in fulfillment_message and "text" in fulfillment_message["text"]:
                 resposta_do_bot = fulfillment_message["text"]["text"][0]
-                historico_de_mensagens.append({"usuario": mensagem, "bot": resposta_do_bot})
+                urls = re.findall(r'(https?://\S+)', resposta_do_bot)
+                for url in urls:
+        # Formato de url
+                   link = f'<a href="{url}">{url}</a>'
+        # Retorna a url em formato de url
+                   resposta_do_bot = resposta_do_bot.replace(url, link)
+                   historico_de_mensagens.append({"usuario": mensagem, "bot": resposta_do_bot})
+            
             else:
                 resposta_do_bot = "Sorry, I didn't understand that."
                 historico_de_mensagens.append({"usuario": mensagem, "bot": resposta_do_bot})
@@ -31,5 +41,7 @@ def botpost(app):
 def botget(app):
     @app.route('/', methods=['GET'])
     def get_mensagem_do_bot():
+        user_id = session_id()
         global historico_de_mensagens
         return render_template('index.html', historico_de_mensagens = historico_de_mensagens)
+    
