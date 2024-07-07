742305f58ce6597a9e90a474694eae03bed64ccb
imrooteodoro <adelsonteodoro67@gmail.com>
2024-07-07 15:00:28 -0300

    fix: corrige o uso da variável user_id para cada requisição get

--- src/services/payload.py
+++ src/services/payload.py
@@ -1,8 +1,8 @@
 import requests
-import routes
 
 
 def send_message_to_dialogflow(mensagem, access_token, user_id):
+    user_id = user_id
     project_id = 'xadrezbot-vtdd' # variavel que amerzena o id do projeto
     url = f"https://dialogflow.googleapis.com/v2/projects/{project_id}/agent/sessions/{user_id}:detectIntent"
     headers = {
