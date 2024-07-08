5af15926cedb6642f3b4e9d5be93e8537b4d7f5d
imrooteodoro <adelsonteodoro67@gmail.com>
2024-07-08 16:17:03 -0300

    feat: cria um modelo para extrair o formato de resposta do bot

---
+++ src/models/bot_message.py
@@ -0,0 +1,18 @@
+from models.format_response import format_bot_response
+
+def extract_bot_response(resultado):
+        if "queryResult" in resultado and "fulfillmentMessages" in resultado["queryResult"]:
+            fulfillment_message = resultado["queryResult"]["fulfillmentMessages"][0]
+
+            if "text" in fulfillment_message and "text" in fulfillment_message["text"]:
+                resposta_do_bot = fulfillment_message["text"]["text"][0]
+                resposta_do_bot = format_bot_response(resposta_do_bot)
+            else:
+                resposta_do_bot = "Sorry, I didn't understand that."
+        else:
+            resposta_do_bot = "Sorry, I didn't understand that."
+
+        return resposta_do_bot
+
+
+
